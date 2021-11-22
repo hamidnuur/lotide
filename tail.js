9f7e914951aa946e93568119c3238939c3d99734
Hamid Nur <hamidsharif2001@gmail.com>
2021-11-22 17:10:10 -0500

    added tail.js

---
+++ tail.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(arr) {
+  return arr.slice(1);
+};
+
+tail(1,2,3);
+assertEqual(1, 1);
