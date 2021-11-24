22a63d2084739c81c69c20e616378f2ddb68818e
Hamid Nur <hamidsharif2001@gmail.com>
2021-11-24 13:32:50 -0500

    completed countLetters

---
+++ countLetters.js
@@ -0,0 +1,37 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(str) {
+
+  const results = {}
+
+  for (var i = 0; i < str.length; i++) {
+    let current = str.charAt(i)
+    if (results[current]) {
+      results[current] += 1;
+    } else {
+      results[current] = 1;
+    }
+  }
+  delete results[" "];
+  return results;
+}
+
+let x = countLetters("lighthouse in the house")
+console.log(x)
+assertEqual(x["l"], 1)
+assertEqual(x["i"], 2)
+assertEqual(x["g"], 1)
+assertEqual(x["h"], 4)
+assertEqual(x["t"], 2)
+assertEqual(x["o"], 2)
+assertEqual(x["u"], 2)
+assertEqual(x["s"], 2)
+assertEqual(x["e"], 3)
+assertEqual(x["n"], 1)
+
