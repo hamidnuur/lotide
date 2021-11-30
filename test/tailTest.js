02f0b45e74a8aca4ce1c740e133c8b12471c80a9
Hamid Nur <hamidsharif2001@gmail.com>
2021-11-29 20:55:23 -0500

    tail and middle using m&c now

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,5 +1,11 @@
-const assertArraysEqual = require('../assertArraysEqual');
-const tail = require('../tail');
+const assert = require('chai').assert;
+const tail   = require('../tail');
 
-//tail(1,2,3);
-assertArraysEqual(tail([1, 2, 3]), [2, 3]);
+describe("#tail", () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']); 
+  });
+});
