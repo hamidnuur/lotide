13e41969397bfe4e7fa8381f96ec803b911fe0e3
Hamid Nur <hamidsharif2001@gmail.com>
2021-11-29 19:35:31 -0500

    refactor head.js, asertEqual.js

--- countLetters.js
+++ countLetters.js
@@ -8,10 +8,10 @@ const assertEqual = function(actual, expected) {
 
 const countLetters = function(str) {
 
-  const results = {}
+  const results = {};
 
   for (var i = 0; i < str.length; i++) {
-    let current = str.charAt(i)
+    let current = str.charAt(i);
     if (results[current]) {
       results[current] += 1;
     } else {
@@ -22,16 +22,16 @@ const countLetters = function(str) {
   return results;
 }
 
-let x = countLetters("lighthouse in the house")
-console.log(x)
-assertEqual(x["l"], 1)
-assertEqual(x["i"], 2)
-assertEqual(x["g"], 1)
-assertEqual(x["h"], 4)
-assertEqual(x["t"], 2)
-assertEqual(x["o"], 2)
-assertEqual(x["u"], 2)
-assertEqual(x["s"], 2)
-assertEqual(x["e"], 3)
-assertEqual(x["n"], 1)
+let x = countLetters("lighthouse in the house");
+
+assertEqual(x["l"], 1);
+assertEqual(x["i"], 2);
+assertEqual(x["g"], 1);
+assertEqual(x["h"], 4);
+assertEqual(x["t"], 2);
+assertEqual(x["o"], 2);
+assertEqual(x["u"], 2);
+assertEqual(x["s"], 2);
+assertEqual(x["e"], 3);
+assertEqual(x["n"], 1);
 
